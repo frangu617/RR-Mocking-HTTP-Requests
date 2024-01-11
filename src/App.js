47e7b6e3afbcbe86e92b953ec40f763de104436e
frangu617 <142462584+frangu617@users.noreply.github.com>
2024-01-10 19:08:33 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+  const [gitHubUrl, setGitHubUrl] = useState('')
+  const [gitHubImg, setGitHubImg] = useState('')
+  const [gitHubRepos, setGitHubRepos] = useState([])
+
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/frangu617')
+      .then(res => res.json())
+      .then(data => {
+        setGitHubName(data.name)
+        setGitHubUrl(data.html_url)
+        setGitHubImg(data.avatar_url)
+        
+
+        fetch(data.repos_url)
+        .then(res => res.json())
+        .then(reposData => {
+          if (Array.isArray(reposData)) {
+            setGitHubRepos(reposData);
+          } else {
+            console.error('Expected an array of repositories', reposData);
+          }
+        })
+        .catch(err => console.error('Error fetching repos:', err));
+      
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href={gitHubUrl}><button style={{margin: '20px'}}>GitHub Profile</button></a>
+
+      <div><img src={gitHubImg} alt="GitHub Profile" /></div>
+
+      <h3>GitHub Repos:</h3>
+      <ul>
+        {gitHubRepos.map(repo => (
+          <li key={repo.id}>
+            <a href={repo.html_url}>{repo.name}</a>
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,35 @@
+import {render } from '@testing-library/react';
+// import { Renderer } from 'react-dom';
+import {screen, waitFor} from '@testing-library/dom'
+import App from './App';
 
+beforeEach(() => {
+    fetch.resetMocks()
+})
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
 
+  test("checks whether the attribute is the correct URL from the GitHub REST API using Jest Fetch Mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({html_url: 'coder'}))
+    render(<App />)
+    const gitHubUrl = await waitFor(() => screen.getByRole('link'))
+    expect(gitHubUrl).toHaveAttribute('href', 'coder')
+  })
 
+  test("checks forthe gitHub profile image to have the correct src attribute", async () => {
+    fetch.mockResponseOnce(JSON.stringify({avatar_url: 'coder'}))
+    render(<App />)
+    const gitHubImg = await waitFor(() => screen.getByRole('img'))
+    expect(gitHubImg).toHaveAttribute('src', 'coder')
+  })
+
+  test("checks for the github repositories for the profile", async () => {
+    fetch.mockResponseOnce(JSON.stringify({repos_url: 'coder'}))
+    render(<App />)
+    const gitHubRepos = await waitFor(() => screen.getByRole('list'))
+    expect(gitHubRepos).toHaveAttribute('href', 'coder')
+  })
